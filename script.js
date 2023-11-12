79240b5bf2c2fe2331f09150b4587959f9966a4b
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-12 09:57:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 //your JS code here. If required.
-function secondHighest(arr) {
- let first = -Infinity;
- let second = -Infinity;
-  
- for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > first) {
-      second = first;
-      first = arr[i];
-    } else if (arr[i] > second && arr[i] < first) {
-      second = arr[i];
-    }
- }
-  
- return second;
-}
+
 
 
